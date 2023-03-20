183f02afb579d0046084d75df64f02c6f27dfce4
sauromates <vs.girenko@mail.ru>
2023-03-20 16:41:58 +0300

    feat: entrypoint updates

--- laravel-entrypoint.sh
+++ laravel-entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 set -e
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- php "$@"
-fi
-
 # Install dependencies
-if [ "$APP_ENV" != 'prod' ]; then
-    composer install --prefer-dist --no-progress --no-interaction
-else
+if [ "$APP_ENV" = 'prod' ]; then
     composer install --no-dev --no-progress --no-interaction
+    php artisan key:generate
+else
+    composer install --prefer-dist --no-progress --no-interaction
 fi
 
 # Run migrations
@@ -18,9 +14,11 @@ if ls -A database/migrations/*.php >/dev/null 2>&1; then
     php artisan migrate --force
 fi
 
+# Build and link frontend resources
 php artisan storage:link --no-interaction
 npm install && npm run build
 
+# Run queue workers via Supervisor
 supervisord -c /etc/supervisord.conf
 
 exec "$@"
